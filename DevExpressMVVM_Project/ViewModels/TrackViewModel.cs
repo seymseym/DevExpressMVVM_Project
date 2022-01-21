9f80cf17a10597e8d06fa0f6e7535ce2bd1a617c
seymseym <seym.kurt97@gmail.com>
2022-01-21 15:57:56 +0300

    Added a ViewModel button binding to View(xaml) file

--- DevExpressMVVM_Project/ViewModels/TrackViewModel.cs
+++ DevExpressMVVM_Project/ViewModels/TrackViewModel.cs
@@ -2,19 +2,39 @@
 using DevExpress.Mvvm.DataAnnotations;
 using DevExpress.Mvvm.POCO;
 using DevExpressMVVM_Project.Data;
+using System;
+using System.Windows.Input;
 
 namespace DevExpressMVVM_Project.ViewModels
 {
     [POCOViewModel]
     public class TrackViewModel : ViewModelBase
     {
+        public ICommand ResetNameCommand { get; set; }
         public virtual TrackInfo Track { get; set; }
 
-        protected TrackViewModel() // Protected constructor
+        public TrackViewModel() // Protected constructor
         {
             // Random Data for Test
             Track = new TrackList()[15]; // Instantiate the property. 15th element of the list.
+            ResetNameCommand = new DelegateCommand(ResetName, CanResetName, true);
         }
+
+        public bool CanResetName()
+        {
+            // It can only be reset if there is a track object assigned to this view model
+            // and if Track.Name property is not empty
+            return Track != null && !String.IsNullOrEmpty(Track.Name);
+        }
+
+        public void ResetName()
+        {
+            if(Track != null)
+            {
+                Track.Name = "";
+            }
+        }
+
         public static TrackViewModel Create()
         {
             return ViewModelSource.Create(() => new TrackViewModel());
