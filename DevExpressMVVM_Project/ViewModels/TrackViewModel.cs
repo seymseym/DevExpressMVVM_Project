88a0b254d8bb13921d38332c1d0bab93f7387b0f
seymseym <seym.kurt97@gmail.com>
2022-01-21 11:26:40 +0300

    Added the ViewModel and View Classes

---
+++ DevExpressMVVM_Project/ViewModels/TrackViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+using DevExpressMVVM_Project.Data;
+
+namespace DevExpressMVVM_Project.ViewModels
+{
+    [POCOViewModel]
+    public class TrackViewModel : ViewModelBase
+    {
+        public virtual TrackInfo Track { get; set; }
+
+        protected TrackViewModel() // Protected constructor
+        {
+            // Random Data for Test
+            Track = new TrackList()[15]; // Instantiate the property. 15th element of the list.
+        }
+        public static TrackViewModel Create()
+        {
+            return ViewModelSource.Create(() => new TrackViewModel());
+        }
+    }
+}
