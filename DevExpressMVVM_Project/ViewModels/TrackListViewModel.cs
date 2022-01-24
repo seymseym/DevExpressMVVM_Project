17acc509e43ca98c0e192bac48fd905d584dfb99
seymseym <seym.kurt97@gmail.com>
2022-01-24 09:18:41 +0300

    Added List View using DXMVVM Grid Control Toolbox

---
+++ DevExpressMVVM_Project/ViewModels/TrackListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.POCO;
+using DevExpressMVVM_Project.Data;
+
+namespace DevExpressMVVM_Project.ViewModels
+{
+    public class TrackListViewModel : ViewModelBase
+    {
+        public virtual TrackList Tracks { get; set; }
+
+        protected TrackListViewModel()
+        {
+            Tracks = new TrackList();
+        }
+
+        //Instantiate this ViewModel through the factory of the framework
+        public static TrackListViewModel Create()
+        {
+            return ViewModelSource.Create(() => new TrackListViewModel());
+        }
+    }
+}
